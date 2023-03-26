ca61bc63b2e2bdd24cb09f6745e085199bdc3b33
PT. TODIS KARYA BANGSA <biput98@gmail.com>
2023-03-26 21:43:00 +0700

    belajar kafka

---
+++ src/test/java/bima/demokafka/Demo/Kafka/DemoKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package bima.demokafka.Demo.Kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
